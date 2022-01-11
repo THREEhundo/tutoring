539680d8c3d102708996115139100e2a805c0495
threehundo <samuelbaik87@gmail.com>
2022-01-11 12:57:20 -0800

    Added dep tailwind, icons, and tailwind theme colors

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import React from 'react';
+import { ELearning } from './assets/index.js';
 import './App.css';
+
+/**
+ * Light Color Palette
+ * teal #85CBCC
+ * light_teal #A8DEE0
+ * khaki #F9E2AE
+ * pale_orange #FBC78D
+ * light_green #A7D676
+ *
+ * Dark Color Palette
+ *midnight #0B0742
+ *navy #120C6E
+ *blue_oyster #5E72EB
+ *coral #FF9190
+ *sunset #FDC094
+ *
+ */
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-700 text-white w-screen h-screen container px-4">
+      <Header />
     </div>
   );
 }
+const Header = () => {
+  return (
+    <header className="flex items-center justify-between px-8 h-24 text-white">
+      <HomeIcon />
+      <Hamburger />
+    </header>
+  );
+};
+
+const HomeIcon = () => (
+  <div className="rounded-full w-fit">
+    <img src={ELearning} alt="home-link" className="w-16 mx-auto" />
+  </div>
+);
+
+const Hamburger = () => (
+  <button className="flex items-center space-y-2 focus:outline-none">
+    <div className="w-6 flex items-center justify-center relative">
+      <span
+        // x-bind:class="open ? 'translate-y-0 rotate-45' : '-translate-y-2'"
+        className="-translate-y-2 transform transition w-full h-px bg-teal absolute"></span>
+      <span
+        // x-bind:class="open ? 'opacity-0 translate-x-3' : 'opacity-100'"
+        className="translate-x-3 transform transition w-full h-px bg-pale-orange absolute"></span>
+
+      <span
+        // x-bind:class="open ? 'translate-y-0 -rotate-45' : 'translate-y-2'"
+        className="translate-y-2 transform transition w-full h-px bg-light-green absolute"></span>
+    </div>
+  </button>
+);
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      // DARK PALETTE
+      midnight: '#0B0742',
+      navy: '#120C6E',
+      'blue-oyster': '#5E72EB',
+      coral: '#FF9190',
+      sunset: '#FDC094',
+      // LIGHT PALETTE
+      teal: '#85CBCC',
+      'light-teal': '#A8DEE0',
+      khaki: '#F9E2AE',
+      'pale-orange': '#FBC78D',
+      'light-green': '#A7D676'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
