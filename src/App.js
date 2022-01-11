e7cfb14230f14124b897c228134f60a2cf2e5149
threehundo <samuelbaik87@gmail.com>
2022-01-11 15:26:21 -0800

    Added Todo List comments

--- src/App.js
+++ src/App.js
@@ -18,43 +18,55 @@ import { ELearning } from './assets/index.js';
  *
  */
 
+/**
+  * 1. Light & Dark Mode
+  *   i.    tailwind config class -> dark 
+  *   ii.   add dark theme to existing components
+  *   iii.  
+  * 2. Finish Header
+  * 3. Prettier - format on save for React
+  *   "[javascriptreact]": {
+        "editor.formatOnSave": true,
+        "editor.defaultFormatter": "esbenp.prettier-vscode"
+      },
+  */
 function App() {
-  return (
-    <div className="bg-white text-light-green w-screen h-screen container px-4">
-      <Header />
-    </div>
-  );
+	return (
+		<div className='bg-white dark:bg-midnight text-light-green dark:text-blue-oyster w-screen h-screen container px-4'>
+			<Header />
+		</div>
+	);
 }
 const Header = () => {
-  return (
-    <header className="flex items-center justify-between px-8 h-24">
-      <HomeIcon />
-      <Hamburger />
-    </header>
-  );
+	return (
+		<header className='flex items-center justify-between px-8 h-24'>
+			<HomeIcon />
+			<Hamburger />
+		</header>
+	);
 };
 
 const HomeIcon = () => (
-  <div className="rounded-full w-fit">
-    <img src={ELearning} alt="home-link" className="w-16 mx-auto" />
-  </div>
+	<div className='rounded-full w-fit'>
+		<img src={ELearning} alt='home-link' className='w-16 mx-auto' />
+	</div>
 );
 
 const Hamburger = () => (
-  <button className="flex items-center space-y-2 focus:outline-none">
-    <div className="w-6 flex items-center justify-center relative">
-      <span
-        // x-bind:class="open ? 'translate-y-0 rotate-45' : '-translate-y-2'"
-        className="-translate-y-2 transform transition w-full h-0.5 bg-teal absolute"></span>
-      <span
-        // x-bind:class="open ? 'opacity-0 translate-x-3' : 'opacity-100'"
-        className="translate-x-3 transform transition w-full h-0.5 bg-pale-orange absolute"></span>
+	<button className='flex items-center space-y-2 focus:outline-none'>
+		<div className='w-6 flex items-center justify-center relative'>
+			<span
+				// x-bind:class="open ? 'translate-y-0 rotate-45' : '-translate-y-2'"
+				className='-translate-y-2 transform transition w-full h-0.5 bg-teal absolute'></span>
+			<span
+				// x-bind:class="open ? 'opacity-0 translate-x-3' : 'opacity-100'"
+				className='translate-x-3 transform transition w-full h-0.5 bg-pale-orange absolute'></span>
 
-      <span
-        // x-bind:class="open ? 'translate-y-0 -rotate-45' : 'translate-y-2'"
-        className="translate-y-2 transform transition w-full h-0.5 bg-light-green absolute"></span>
-    </div>
-  </button>
+			<span
+				// x-bind:class="open ? 'translate-y-0 -rotate-45' : 'translate-y-2'"
+				className='translate-y-2 transform transition w-full h-0.5 bg-light-green absolute'></span>
+		</div>
+	</button>
 );
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { ThemeProvider } from './color-theme/ThemeContext';
+import Background from './color-theme/Background';
+import Toggle from './color-theme/ThemeToggle';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<ThemeProvider>
+			<Background>
+				<Toggle />
+				<App />
+			</Background>
+		</ThemeProvider>
+	</React.StrictMode>,
+	document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
