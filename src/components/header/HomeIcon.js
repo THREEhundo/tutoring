033c32b8266101f0dc4bedf27c0a653f347fa95d
threehundo <samuelbaik87@gmail.com>
2022-01-13 10:17:24 -0800

    Fixed hamburger close icon

--- src/components/header/HomeIcon.js
+++ src/components/header/HomeIcon.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { ELearning } from '../../assets/index.js';
 
 const HomeIcon = () => (
-	<div className='rounded-full w-fit'>
+	<div>
 		<img src={ELearning} alt='home-link' className='w-16 mx-auto' />
 	</div>
 );
--- src/components/header/ThemeToggle.js
+++ src/components/header/ThemeToggle.js
@@ -1,21 +1,21 @@
 import React, { useContext } from 'react';
 import { FaSun, FaMoon } from 'react-icons/fa';
-import { ThemeContext } from './ThemeContext';
+import { ThemeContext } from '../../color-theme/ThemeContext';
 
 const Toggle = () => {
 	const { theme, setTheme } = useContext(ThemeContext);
 
 	return (
-		<div className='transition duration-500 ease-in-out rounded-full p-2'>
+		<div className='transition duration-500 ease-in-out rounded-full'>
 			{theme === 'dark' ? (
 				<FaSun
 					onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
-					className='hover:animate-spin mr-5 text-gray-500 dark:text-gray-400 text-2xl cursor-pointer fill-khaki'
+					className='hover:animate-spin mr-1 text-gray-500 dark:text-gray-400 text-2xl cursor-pointer fill-khaki'
 				/>
 			) : (
 				<FaMoon
 					onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
-					className='hover:fill-yellow-400 mr-5 text-gray-500 dark:text-gray-400 text-2xl cursor-pointer fill-sunset stroke-midnight stroke-10'
+					className='hover:fill-yellow-400 mr-1 text-gray-500 dark:text-gray-400 text-2xl cursor-pointer fill-sunset stroke-midnight stroke-10'
 				/>
 			)}
 		</div>
