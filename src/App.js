586a010df788058b015e28e6dd360eaf5f26937d
THREEhundo <samuelbaik87@gmail.com>
2022-03-21 17:28:28 -0700

    Logo test

--- src/App.js
+++ src/App.js
@@ -1,46 +1,46 @@
-import React, { useState } from "react";
-import { Outlet, Route, Routes } from "react-router-dom";
-import { ContactModal } from "./components/contact-modal/ContactModal";
-import { Footer } from "./components/footer/Footer";
-import { Header, NavLinks } from "./components/header/Header";
-import { Intro } from "./components/intro-page/Intro";
-import { Testimonials } from "./components/testimonials/Testimonials";
-import { What } from "./components/everything/Everything";
-import { Who } from "./components/who-page/Who";
+import React, { useState } from 'react'
+import { Outlet, Route, Routes } from 'react-router-dom'
+import { ContactModal } from './components/contact-modal/ContactModal'
+import { Footer } from './components/footer/Footer'
+import { Header, NavLinks } from './components/header/Header'
+import { Intro } from './components/intro-page/Intro'
+import { Testimonials } from './components/testimonials/Testimonials'
+import { What } from './components/everything/Everything'
+import { Who } from './components/who-page/Who'
 
 // TODO: Add Contact
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Wrapper />}>
-        <Route index element={<Intro />} />
-        <Route path="/about" element={<Who />} />
-        <Route path="/everything" element={<What />} />
-        <Route path="/testimonials" element={<Testimonials />} />
-        {/*<Route path="/contact" element={<ContactModal />} /> */}
-        <Route path="*" element={<NoMatch />} />
-      </Route>
-    </Routes>
-  );
+	return (
+		<Routes>
+			<Route path='/' element={<Wrapper />}>
+				<Route index element={<Intro />} />
+				<Route path='/about' element={<Who />} />
+				<Route path='/everything' element={<What />} />
+				<Route path='/testimonials' element={<Testimonials />} />
+				{/*<Route path="/contact" element={<ContactModal />} /> */}
+				<Route path='*' element={<NoMatch />} />
+			</Route>
+		</Routes>
+	)
 }
-export default App;
+export default App
 
 const Wrapper = () => {
-  const [openMenu, setMenu] = useState(false);
-  const handleClick = () => setMenu(!openMenu);
+	const [openMenu, setMenu] = useState(false)
+	const handleClick = () => setMenu(!openMenu)
 
-  return (
-    <div className="bg-pale-orange text-center text-sunset scroll-smooth min-h-screen">
-      <NavLinks openMenu={openMenu} handleClick={handleClick} />
-      <Header openMenu={openMenu} handleClick={handleClick} />
-      <Outlet />
-      <Footer />
-    </div>
-  );
-};
+	return (
+		<div className='bg-pale-orange text-center text-sunset scroll-smooth min-h-screen'>
+			<NavLinks openMenu={openMenu} handleClick={handleClick} />
+			<Header openMenu={openMenu} handleClick={handleClick} />
+			<Outlet />
+			<Footer />
+		</div>
+	)
+}
 
 const NoMatch = () => (
-  <main style={{ padding: "1rem" }}>
-    <p>There's nothing here!</p>
-  </main>
-);
+	<main style={{ padding: '1rem' }}>
+		<p>There's nothing here!</p>
+	</main>
+)
--- src/components/intro-page/Intro.jsx
+++ src/components/intro-page/Intro.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import { ELearning } from "../../assets/index";
-import { ReactComponent as Zoom } from "../../assets/icons/zoom.svg";
-import { CgArrowLongRight } from "react-icons/cg";
-import { DesktopContainer } from "../generic/DesktopContainer";
-import { HugeHeading } from "../generic/HugeHeading";
-import metalicaGirl from "../../assets/images/pexels-dillon-kydd-5794559.jpeg";
-import bucketsGirl from "../../assets/images/pexels-dominika-roseclay-1172784.jpeg";
-import euniceBaikLogo from "../../assets/euniceBaikLogo.mp4";
+import React from 'react'
+import { ELearning } from '../../assets/index'
+import { ReactComponent as Zoom } from '../../assets/icons/zoom.svg'
+import { CgArrowLongRight } from 'react-icons/cg'
+import { DesktopContainer } from '../generic/DesktopContainer'
+import { HugeHeading } from '../generic/HugeHeading'
+import metalicaGirl from '../../assets/images/pexels-dillon-kydd-5794559.jpeg'
+import bucketsGirl from '../../assets/images/pexels-dominika-roseclay-1172784.jpeg'
+import euniceBaikLogo from '../../assets/euniceBaikLogo.mp4'
 
 /**
  * ! Intro
@@ -17,36 +17,76 @@ import euniceBaikLogo from "../../assets/euniceBaikLogo.mp4";
 
 // TODO: Snap container ==> <Intro /> snap-mandatory scroll-smooth snap-y snap-mandatory overflow-scroll
 export const Intro = () => (
-  <section className="w-full text-light-red">
-    <IntroSplash />
-    <IntroMainContentContainer />
-    <ViewTestimonials />
-  </section>
-);
+	<section className='w-full text-light-red'>
+		<IntroSplash />
+		<IntroMainContentContainer />
+		<ViewTestimonials />
+	</section>
+)
 
 // TODO: Snap Item ==> <IntroSplash />  snap-always snap-start
 const IntroSplash = ({ children }) => {
-  return (
-    <section id="intro" className="flex flex-col items-center">
-      {children}
-      <IntroContent />
-    </section>
-  );
-};
+	return (
+		<section id='intro' className='flex flex-col items-center'>
+			{children}
+			<IntroContent />
+		</section>
+	)
+}
 // ! Fixing !
 // ! Figure out passing classes to SingleColumn and HugeHeader
 
 const IntroContent = () => (
-  <main
-    id="landing"
-    className="flex flex-col flex-grow justify-center h-screen min-h-full m-auto items-center"
-  >
-    {/*<HugeHeading>Eunice Baik</HugeHeading>*/}
-    <video playsInline autoPlay muted loop className="z-1 w-3/4 object-cover">
-      <source src={euniceBaikLogo} type="video/mp4" id="loop"></source>
-    </video>
-  </main>
-);
+	<main
+		id='landing'
+		className='flex flex-col flex-grow justify-center min-h-full m-auto items-center'>
+		{/*^^^ put h-screen back after testing*/}
+		<p>1250</p>
+		<video
+			playsInline
+			autoPlay
+			muted
+			loop
+			className='z-1 w-[1250px] object-cover'>
+			<source src={euniceBaikLogo} type='video/mp4' id='loop'></source>
+		</video>
+		<p>1000px</p>
+		<video
+			playsInline
+			autoPlay
+			muted
+			loop
+			className='z-1 w-[1000px] object-cover'>
+			<source src={euniceBaikLogo} type='video/mp4' id='loop'></source>
+		</video>
+		<p>900px</p>
+		<video
+			playsInline
+			autoPlay
+			muted
+			loop
+			className='z-1 w-[900px] object-cover'>
+			<source src={euniceBaikLogo} type='video/mp4' id='loop'></source>
+		</video>
+		<video
+			playsInline
+			autoPlay
+			muted
+			loop
+			className='z-1 w-[800px] object-cover'>
+			<source src={euniceBaikLogo} type='video/mp4' id='loop'></source>
+		</video>
+		<p>700px</p>
+		<video
+			playsInline
+			autoPlay
+			muted
+			loop
+			className='z-1 w-[700px] object-cover'>
+			<source src={euniceBaikLogo} type='video/mp4' id='loop'></source>
+		</video>
+	</main>
+)
 
 /**
  * ! Main Content
@@ -56,102 +96,100 @@ const IntroContent = () => (
 
 // TODO: Snap Item ==> <IntroMainContentContainer /> snap-always snap-start
 export const IntroMainContentContainer = () => (
-  <section
-    id="main-content"
-    className="flex flex-grow justify-center min-h-full h-screen"
-  >
-    <DesktopContainer addClasses={"flex flex-col items-center"}>
-      <Description />
-    </DesktopContainer>
-  </section>
-);
+	<section
+		id='main-content'
+		className='flex flex-grow justify-center min-h-full h-screen'>
+		<DesktopContainer addClasses={'flex flex-col items-center'}>
+			<Description />
+		</DesktopContainer>
+	</section>
+)
 
 const Description = () => (
-  <section id="descriptive-content" className="flex flex-col flex-grow">
-    <h3 className="text-xl font-bold my-5">POCKET ROCKET</h3>
-    <span>|||</span>
-    <h2 className="text-3xl font-bold mt-7 mb-3">
-      Eunice is a Yale University graduate awaiting acceptance into medical
-      school.
-    </h2>
-    <p className="px-16">
-      Top of her class at every stage of her journey through education, she
-      imparts knowledge on specific subjects and most important of all, learning
-      how to learn.
-    </p>
-  </section>
-);
+	<section id='descriptive-content' className='flex flex-col flex-grow'>
+		<h3 className='text-xl font-bold my-5'>POCKET ROCKET</h3>
+		<span>|||</span>
+		<h2 className='text-3xl font-bold mt-7 mb-3'>
+			Eunice is a Yale University graduate awaiting acceptance into medical
+			school.
+		</h2>
+		<p className='px-16'>
+			Top of her class at every stage of her journey through education, she
+			imparts knowledge on specific subjects and most important of all, learning
+			how to learn.
+		</p>
+	</section>
+)
 
 // TODO: made page 100vh
 // TODO: Snap Item ==> <ViewTestimonials /> snap-always snap-start
 const ViewTestimonials = () => (
-  <section
-    id="testimonial-teaser-link"
-    className="flex min-h-full flex-col h-screen  items-center justify-center relative text-sunset"
-  >
-    {/* mb-96 */}
-    <h2 className="text-7xl font-bold mt-20 mb-3">Testimonials</h2>
-    <a className="" href="#testimonials">
-      <span className="border-solid border border-light-teal py-2">
-        <RightArrow />
-      </span>
-      <span className="border-solid border border-light-teal py-2 px-1 underline decoration-light-teal">
-        VIEW TESTIMONIALS
-      </span>
-      <span className="border-solid border border-light-teal py-2">
-        <RightArrow />
-      </span>
-    </a>
-    <ZoomContainer />
-    <StaggeredImgContainer />
-  </section>
-);
+	<section
+		id='testimonial-teaser-link'
+		className='flex min-h-full flex-col h-screen  items-center justify-center relative text-sunset'>
+		{/* mb-96 */}
+		<h2 className='text-7xl font-bold mt-20 mb-3'>Testimonials</h2>
+		<a className='' href='#testimonials'>
+			<span className='border-solid border border-light-teal py-2'>
+				<RightArrow />
+			</span>
+			<span className='border-solid border border-light-teal py-2 px-1 underline decoration-light-teal'>
+				VIEW TESTIMONIALS
+			</span>
+			<span className='border-solid border border-light-teal py-2'>
+				<RightArrow />
+			</span>
+		</a>
+		<ZoomContainer />
+		<StaggeredImgContainer />
+	</section>
+)
 
 // TODO: mt-20 ml-48
 const ZoomContainer = () => (
-  <div className="">
-    <Zoom className="w-40" />
-  </div>
-);
+	<div className=''>
+		<Zoom className='w-40' />
+	</div>
+)
 
 /* Vertically Align text with SVG's */
-const RightArrow = () => <CgArrowLongRight className="inline-flex w-10" />;
+const RightArrow = () => <CgArrowLongRight className='inline-flex w-10' />
 
 // TODO: HOVER OVER IMG SHOW TEXT BUBBLE
 // TODO: ON SCROLL SHOW TESTIMONIAL TEXT
 const StaggeredImgContainer = () => (
-  <section className="flex absolute bottom-0 h-full">
-    <TopLeftCard />
-    <BttmRightCard />
-  </section>
-);
+	<section className='flex absolute bottom-0 h-full'>
+		<TopLeftCard />
+		<BttmRightCard />
+	</section>
+)
 
 const TopLeftCard = () => (
-  <section className="flex justify-start flex-col items-center my-10 relative">
-    <p className="w-1/2 bg-coral text-navy rounded-lg absolute -right-10 top-16">
-      Eunice helped me get into Harvard with a 5.9 GPA!
-    </p>
-    <img
-      src={metalicaGirl}
-      alt="testimonal headshot left side"
-      className="w-1/2 aspect-[640/906] border-8 border-x-4 border-coral rounded-full"
-    />
-  </section>
-);
+	<section className='flex justify-start flex-col items-center my-10 relative'>
+		<p className='w-1/2 bg-coral text-navy rounded-lg absolute -right-10 top-16'>
+			Eunice helped me get into Harvard with a 5.9 GPA!
+		</p>
+		<img
+			src={metalicaGirl}
+			alt='testimonal headshot left side'
+			className='w-1/2 aspect-[640/906] border-8 border-x-4 border-coral rounded-full'
+		/>
+	</section>
+)
 
 const BttmRightCard = () => (
-  <section className="flex justify-end flex-col items-center my-10 relative">
-    <img
-      src={bucketsGirl}
-      alt="testimonal headshot right side"
-      className="self-center w-1/2 mt-36 aspect-[640/906] border-8 border-x-4 border-blue-oyster rounded-full"
-    />
-    {/* TODO: FIX POSITIONING FOR SMALLER SCREENS */}
-    <p className="w-1/2 bg-blue-oyster text-navy rounded-lg absolute -left-20 bottom-16">
-      Eunice is worth her weight in GOLD!
-    </p>
-  </section>
-);
+	<section className='flex justify-end flex-col items-center my-10 relative'>
+		<img
+			src={bucketsGirl}
+			alt='testimonal headshot right side'
+			className='self-center w-1/2 mt-36 aspect-[640/906] border-8 border-x-4 border-blue-oyster rounded-full'
+		/>
+		{/* TODO: FIX POSITIONING FOR SMALLER SCREENS */}
+		<p className='w-1/2 bg-blue-oyster text-navy rounded-lg absolute -left-20 bottom-16'>
+			Eunice is worth her weight in GOLD!
+		</p>
+	</section>
+)
 /** 
  * ! TESTIMONIAL ANCHOR EXAMPLE
  * <a
